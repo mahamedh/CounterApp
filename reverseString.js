af48d71c91354b26a07189642c614ce1da4a8597
Murshid Ahamed <murshid.ahamed@pearson.com>
2022-04-27 16:34:46 +0530

    update

--- reverseString.js
+++ reverseString.js
@@ -1,13 +1,14 @@
 
-
-
 function reverseString(str) {
+
   const newString = "";
 
   //loop the string provided by last index and assigned to empty string of first index
   for (let i = str.length - 1; i >= 0; i--) {
-      
-    newString += str[i];
+
+       newString += str[i];
+
   }
+
   return newString;
 }

32862171c13480cf85caad6c880e9179a37fb72e
Murshid Ahamed <murshid.ahamed@pearson.com>
2022-04-27 16:17:51 +0530

    update

--- reverseString.js
+++ reverseString.js
@@ -1,11 +1,10 @@
- 
-const array = ["A","B","C","D"]; 
-var rarray = [];
 
-  const reverseArray = () => {
+const array = ["A", "B", "C", "D"];
+const rarray = [];
 
-    rarray = array.map((item, idx) => array[array.length - 1 - idx]);
-    
-  }
- reverseArray();
- console.log(rarray);
+function reverseString(array) {
+
+  rarray = array.map((item, idx) => array[array.length - 1 - idx]);
+  return rarray;
+}
+

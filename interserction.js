ccd5e6d5a59fda6bfd1d54cc8779ba36210b870e
Murshid Ahamed <murshid.ahamed@pearson.com>
2022-04-27 10:18:47 +0530

    challange 03

---
+++ interserction.js
@@ -0,0 +1,62 @@
+ 
+ var arr1 = [45, 99, 55, 17, 93, 23];
+ var arr2 = [45, 18, 93, 7, 17];
+
+function getCommon(arr1, arr2) {
+
+  arr1.sort();                      
+  arr2.sort();
+  var common = [];                
+  var i = 0, j = 0;                
+  
+  while(i<arr1.length && j<arr2.length) {
+    
+    if(arr1[i] == arr2[j]) {       
+      common.push(arr1[i]);
+      i++;
+      j++;
+    }
+    else if(arr1[i] < arr2[j]) {  
+      i++;                      
+    }                             
+    else {
+      j++;
+    }
+  }
+  console.log(common);
+  return common;
+}
+
+
+Array.prototype.getCommon = function(array) { 
+
+  this.sort();                      
+  arr2.sort();
+  var common = [];                
+  var i = 0, j = 0;                
+  
+  while(i<this.length && j<arr2.length) {
+    
+    if(this[i] == arr2[j]) {        
+      common.push(this[i]);
+      i++;
+      j++;
+    }
+    else if(this[i] < arr2[j]) {  
+      i++;                        
+    }                            
+    else {
+      j++;
+    }
+  }
+  console.log(common)
+  return common;
+
+}
+
+const first=getCommon(arr1,arr2);
+const second=arr1.getCommon(arr2);
+
+
+console.log(first);
+console.log(second);
